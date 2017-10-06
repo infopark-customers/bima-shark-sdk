af3faad7e6a06665571b2b6cf7c8691f33aff3ef
Joergen Dahlke <joergen.dahlke@infopark.de>
2017-10-06 09:50:24 +0200

    meta #add_datetime_accessors to help datetime attributes

--- lib/shark/base.rb
+++ lib/shark/base.rb
@@ -9,5 +9,26 @@ module Shark
         'Accept' => 'application/vnd.api+json'
       }
     }
+
+    class << self
+      def add_datetime_accessors(*method_names)
+        method_names.each do |name|
+          # get
+          define_method name do
+            value = super()
+            DateTime.parse(value)  if value
+          end
+
+          # set
+          define_method "#{name}=" do |value|
+            if value.respond_to?(:iso8601)
+              super(value.iso8601(0))
+            else
+              super(value)
+            end
+          end
+        end
+      end
+    end
   end
 end

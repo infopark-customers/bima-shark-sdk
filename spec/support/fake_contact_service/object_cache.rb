9836314a062c6eaad0a41abe51bde01489fd268f
Johannes Schmeißer <johanneszsolt@gmail.com>
2017-11-08 21:00:50 +0100

    removed factories

--- spec/support/fake_contact_service/object_cache.rb
+++ spec/support/fake_contact_service/object_cache.rb
@@ -14,28 +14,5 @@ module FakeContactService
     def add(object)
       objects.push(object)
     end
-
-    def json_body(id)
-      object = objects.detect { |o| o.attributes["id"].to_s == id.to_s }
-
-      p "****jbody called"
-      p object
-
-       x = {
-        data: {
-          id: object["id"],
-          type: "groups",
-          attributes: {
-            title: object["title"],
-            members_can_admin: object["members_can_admin"]
-          }
-        }
-      }
-
-      p "*** x:"
-      p x
-
-      x
-    end
   end
 end

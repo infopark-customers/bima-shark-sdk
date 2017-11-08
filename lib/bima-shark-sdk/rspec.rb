384cab3eabcede5cc02ac0c289571dfc16143fcc
Johannes Schmeißer <johanneszsolt@gmail.com>
2017-11-08 21:17:44 +0100

    require spec support files only

--- lib/bima-shark-sdk/rspec.rb
+++ lib/bima-shark-sdk/rspec.rb
@@ -1,4 +1,4 @@
-Dir[File.join(File.dirname(__dir__), "..", "spec", "**","*.rb")].each { |f| require f }
+Dir[File.join(File.dirname(__dir__), "..", "spec", "support", "**","*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = ".rspec_status"

bec8129ee73d5b287ee0be018f29af782ab37c08
Johannes Schmeißer <johanneszsolt@gmail.com>
2017-11-09 04:12:33 +0100

    code cleaning in rspec.rb

--- lib/bima-shark-sdk/rspec.rb
+++ lib/bima-shark-sdk/rspec.rb
@@ -1,11 +1,5 @@
 Dir[File.join(File.dirname(__dir__), "..", "spec", "support", "**","*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
-  config.example_status_persistence_file_path = ".rspec_status"
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
-
   config.include ContactServiceHelper
 end

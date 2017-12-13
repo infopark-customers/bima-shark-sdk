fb6f54e41e40a0dbe961ba1003a4df98bd31df30
Joergen Dahlke <joergen.dahlke@infopark.de>
2017-12-13 17:01:23 +0100

    cleanup shark configuration

--- lib/shark.rb
+++ lib/shark.rb
@@ -1,62 +1,51 @@
-require 'active_support/all'
-require 'bima-http'
-require 'json_api_client'
+require "logger"
+require "forwardable"
+require "active_support/all"
+require "bima-http"
+require "json_api_client"
 
-require 'shark/client/connection'
-require 'shark/middleware/status'
+require "shark/version"
+require "shark/configuration"
+require "shark/error"
+require "shark/client/connection"
+require "shark/middleware/status"
+require "shark/base"
 
-require 'shark/base'
+require "shark/form_service/base"
+require "shark/form_service/form"
+require "shark/form_service/structure"
+require "shark/form_service/user_input"
 
-require 'shark/form_service/base'
-require 'shark/form_service/configuration'
-require 'shark/form_service/form'
-require 'shark/form_service/structure'
-require 'shark/form_service/user_input'
+require "shark/survey_service/base"
+require "shark/survey_service/participant"
+require "shark/survey_service/survey"
 
-require 'shark/survey_service/base'
-require 'shark/survey_service/configuration'
-require 'shark/survey_service/participant'
-require 'shark/survey_service/survey'
-
-require 'shark/contact_service/concerns/normalized_email'
-require 'shark/contact_service/base'
-require 'shark/contact_service/configuration'
-require 'shark/contact_service/group'
-require 'shark/contact_service/account'
-require 'shark/contact_service/contact'
-
-require 'shark/configuration'
-require 'shark/errors'
-require 'shark/version'
+require "shark/contact_service/concerns/normalized_email"
+require "shark/contact_service/base"
+require "shark/contact_service/group"
+require "shark/contact_service/account"
+require "shark/contact_service/contact"
 
 module Shark
-  mattr_accessor :configuration, :logger
+  extend SingleForwardable
+
+  def_single_delegators :configuration, :cache, :logger, :with_service_token, :service_token
+
+  mattr_accessor :configuration
 
   def self.configure
     self.configuration ||= Configuration.new
     yield self.configuration
   end
 
-  self.logger = ::Logger.new(STDOUT)
-  logger.formatter = proc do |_severity, _datetime, _progname, msg|
-    "#{msg}\n"
-  end
-
-  # @api public
-  def self.with_service_token(service_token)
-    self._service_token = service_token
-    yield
-  ensure
-    self._service_token = nil
-  end
-
-  # @api private
-  def self._service_token
-    Thread.current["shark-service-token"]
-  end
-
-  # @api private
-  def self._service_token=(value)
-    Thread.current["shark-service-token"] = value
+  #
+  # Initial configuration
+  #
+  self.configure do |config|
+    config.cache = ActiveSupport::Cache::NullStore.new
+    config.logger = ::Logger.new("/dev/null")
+    config.logger.formatter = proc do |severity, datetime, progname, msg|
+      "#{severity}, #{msg}\n"
+    end
   end
 end
--- lib/shark/client/connection.rb
+++ lib/shark/client/connection.rb
@@ -5,7 +5,7 @@ module Shark
 
       def initialize(options = {})
         @site = options.fetch(:site)
-        @connection_options = options.slice(:app_name, :params)
+        @connection_options = {}
         @connection_options[:headers] = options.fetch(:headers)
 
         @connection = Faraday.new do |faraday|
@@ -18,8 +18,8 @@ module Shark
       def use(middleware, *args, &block); end
 
       def run(action, path, params = {}, headers = {})
-        raise ArgumentError, 'Configuration :site cannot be nil' if site.blank?
-        raise ArgumentError, 'Parameter :path cannot be nil' if path.blank?
+        raise ArgumentError, "Configuration :site cannot be nil"  if site.blank?
+        raise ArgumentError, "Parameter :path cannot be nil"      if path.blank?
 
         url = File.join(site, path)
         options = request_options(params, headers)
@@ -32,11 +32,11 @@ module Shark
       def request_options(params = {}, headers = {})
         options = connection_options.reverse_merge(params: {}, headers: {})
         options[:headers] = options[:headers].merge(headers)
-        if Shark._service_token.present?
-          options[:headers].merge!('Authorization' => "Bearer #{Shark._service_token}")
-        end
         options[:params] = options[:params].merge(params)
         options[:connection] = @connection
+        if Shark.service_token.present?
+          options[:headers]["Authorization"] = "Bearer #{Shark._service_token}"
+        end
 
         options
       end

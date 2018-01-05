763ce5f072c6409e10ea3272ecf306124a535b5d
Joergen Dahlke <joergen.dahlke@infopark.de>
2018-01-05 16:36:23 +0100

    add FormService::V2::FormVersion with parsed form structure

--- lib/shark/form_service.rb
+++ lib/shark/form_service.rb
@@ -1,4 +1,11 @@
 require "shark/form_service/base"
+require "shark/form_service/form/element"
+require "shark/form_service/form/container"
+require "shark/form_service/form/multiple_choice"
+require "shark/form_service/form/rating_scale"
+require "shark/form_service/form/text_area"
+require "shark/form_service/form/structure"
+
 require "shark/form_service/v2/base"
 require "shark/form_service/v2/form"
 require "shark/form_service/v2/form_input"
---
+++ lib/shark/form_service/v2/form_version.rb
@@ -0,0 +1,17 @@
+module Shark
+  module FormService
+    module V2
+      class FormVersion < V2::Base
+        belongs_to :form
+
+        def self.table_name
+          "versions"
+        end
+
+        def form_structure
+          @form_structure ||= FormService::Form::Structure.new(structure)
+        end
+      end
+    end
+  end
+end

36cadbcdb716fe12039acfb1a157dc39fb1939df
Adam Musial-Bright <adam.musialbright@googlemail.com>
2018-03-28 14:52:04 +0200

    text field attribute with easy pre and post text accessor

--- lib/shark/form_service/form/text_field.rb
+++ lib/shark/form_service/form/text_field.rb
@@ -2,6 +2,15 @@ module Shark
   module FormService
     module Form
       class TextField < Element
+
+        def pre_text
+          attribute_definition("pre_text")["value"]
+        end
+
+        def post_text
+          attribute_definition("post_text")["value"]
+        end
+
       end
     end
   end

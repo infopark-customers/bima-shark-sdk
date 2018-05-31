20417fafbf96dd1084744a8429bf9cdd628a396d
natalka <ntyszkiewicz@gmail.com>
2018-05-31 12:25:06 +0200

    Simplify Notification model

--- lib/shark/notification_service/notification.rb
+++ lib/shark/notification_service/notification.rb
@@ -1,8 +1,6 @@
 module Shark
   module NotificationService
     class Notification < Base
-      belongs_to :user
-
       custom_endpoint :read_all, on: :collection, request_method: :patch
 
     end

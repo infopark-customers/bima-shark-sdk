54138fa49a54d04a32f4b8911b1e2ba8965a9ec4
natalka <ntyszkiewicz@gmail.com>
2018-07-17 15:36:45 +0200

    Add FakeNotificationService

---
+++ lib/shark/rspec/fake_notification_service.rb
@@ -0,0 +1,12 @@
+require_relative "fake_notification_service/request"
+
+module Shark
+  module RSpec
+    module FakeNotificationService
+      def self.setup
+        Request.setup
+      end
+
+    end
+  end
+end

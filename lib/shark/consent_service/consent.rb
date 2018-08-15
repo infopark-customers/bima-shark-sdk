00111b6b70a70af3af5d220e1d4a511ad2f9da04
Johannes Schmeißer <johanneszsolt@gmail.com>
2018-08-15 14:35:25 +0200

    disabled some consent actions

--- lib/shark/consent_service/consent.rb
+++ lib/shark/consent_service/consent.rb
@@ -1,6 +1,25 @@
 module Shark
   module ConsentService
     class Consent < Base
+      def self.all
+        raise Shark::ActionNotSupportedError, "Shark::ConsentService::Consent.all is not supported"
+      end
+
+      def update_attributes(attributes = {})
+        raise Shark::ActionNotSupportedError, "Shark::ConsentService::Consent#update_attributes is not supported"
+      end
+
+      def destroy
+        raise Shark::ActionNotSupportedError, "Shark::ConsentService::Consent#destroy is not supported"
+      end
+
+      def save
+        if self["id"].present?
+          raise Shark::ActionNotSupportedError, "Shark::ConsentService::Consent#save is not supported for persisted consents"
+        else
+          super
+        end
+      end
     end
   end
 end

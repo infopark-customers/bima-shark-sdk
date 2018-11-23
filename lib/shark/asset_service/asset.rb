3155cff81b2d707c65839ccaa201b6a772fa9798
Grzegorz Jakubiak <grzegorz.jakubiak@infopark.de>
2018-11-23 11:03:34 +0100

    Add recreate_variations and raise error when using where

--- lib/shark/asset_service/asset.rb
+++ lib/shark/asset_service/asset.rb
@@ -1,6 +1,11 @@
 module Shark
   module AssetService
     class Asset < Base
+      custom_endpoint :recreate_variations, on: :member, request_method: :post
+
+      def self.where(args = {})
+        raise Shark::ActionNotSupportedError, "Shark::AssetService::Asset.where is not supported"
+      end
     end
   end
 end

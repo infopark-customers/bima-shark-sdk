01c75dc611df7e0dc88aaba4c9d8c64c31b34227
Johannes Schmeißer <johanneszsolt@gmail.com>
2018-11-26 03:37:12 +0100

    double opt in: minor changes

--- lib/shark/double_opt_in_service/execution.rb
+++ lib/shark/double_opt_in_service/execution.rb
@@ -32,43 +32,6 @@ module Shark
           public_send("#{key}=", data["attributes"][key])
         end
       end
-
-
-
-
-      # custom_endpoint :verify, on: :member, request_method: :post
-      #
-      # def self.verify(verification_token)
-      #   execution = find(verification_token).first
-      #   execution.verify.first
-      # end
-      #
-      # def self.close(verification_token)
-      #   execution = find(verification_token).first
-      #   execution_attributes = execution.attributes.dup
-      #   execution.destroy
-      #   new(execution_attributes)
-      # end
-      #
-      # def self.create(attributes)
-      #   raise Shark::ActionNotSupportedError, "Shark::DoubleOptInService::Execution.create is not supported"
-      # end
-      #
-      # def self.all
-      #   raise Shark::ActionNotSupportedError, "Shark::DoubleOptInService::Execution.all is not supported"
-      # end
-      #
-      # def update_attributes(attributes = {})
-      #   raise Shark::ActionNotSupportedError, "Shark::DoubleOptInService::Execution#update_attributes is not supported"
-      # end
-      #
-      # def save
-      #   if self["id"].present?
-      #     raise Shark::ActionNotSupportedError, "Shark::DoubleOptInService::Execution#save is not supported for persisted executions"
-      #   else
-      #     super
-      #   end
-      # end
     end
   end
 end

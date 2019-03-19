e383f4c7ebe1b5ee18bb1d05c7b8d2479f9c6398
Joergen Dahlke <joergen.dahlke@infopark.de>
2019-03-19 10:53:57 +0100

    JsonApiClient compatibility for <> 1.5

--- lib/shark/client/connection.rb
+++ lib/shark/client/connection.rb
@@ -1,14 +1,14 @@
 module Shark
   module Client
     class Connection
-      HTTP_METHODS_WITH_BODY = [:post, :patch, :put]
-
       attr_reader :site, :connection_options
 
       def initialize(options = {})
         @site = options.fetch(:site)
-        @connection_options = {}
-        @connection_options[:headers] = options.fetch(:headers)
+        @connection_options = {
+          headers: options[:headers] || {},
+          params: options[:params] || {}
+        }
 
         @connection = Faraday.new do |faraday|
           faraday.use Shark::Middleware::ComposeRequest
@@ -20,43 +20,69 @@ module Shark
 
       def use(middleware, *args, &block); end
 
-      # @param action [Symbol] One of :get, :post, :put, :patch, :delete.
+      # @param request_action [Symbol] One of :get, :post, :put, :patch, :delete.
       # @param path [String] The url path
-      # @param params [Hash] The parameters for query or body.
-      # @param headers [Hash] The request headers
+      # @param options [Hash]
+      #   - params [Hash] The request params
+      #   - headers [Hash] The request headers
+      #   - body [Hash]
       # @return [Faraday::Response]
-      # @api public
-      def run(action, path, params: {}, headers: {}, body: nil)
-        raise ArgumentError, "Configuration :site cannot be nil"  if site.blank?
-        raise ArgumentError, "Parameter :path cannot be nil"      if path.blank?
+      # @api private
+      def __run(request_action, path, params: {}, headers: {}, body: nil)
+        raise ArgumentError, "Configuration :site cannot be nil" if site.blank?
+        raise ArgumentError, "Parameter :path cannot be nil" if path.blank?
 
         url = File.join(site, path)
-        headers = request_headers(headers)
-        params = request_params(params)
+        request_headers = connection_options_headers.merge(headers || {})
+        request_params = connection_options_params.merge(params || {})
 
-        @connection.send(action) do |request|
-          request.url(url)
-          request.headers.merge!(headers)
-          request.body = body
-          request.params.merge!(params)
+        if Shark.service_token.present?
+          request_headers["Authorization"] = "Bearer #{Shark.service_token}"
         end
+
+        @connection.send(request_action) do |request|
+          request.url(url)
+          request.headers.merge!(request_headers)
+          request.body = body
+          request.params.merge!(request_params)
+        end
+      end
+
+
+      # For compatibility with older versions of JsonApiClient,
+      # that are used in some projects.
+      if JsonApiClient::VERSION < '1.6'
+        # @param request_action [Symbol] One of :get, :post, :put, :patch, :delete.
+        # @param path [String] The url path
+        # @param params [Hash] The parameters for query or body.
+        # @param headers [Hash] The request headers
+        # @return [Faraday::Response]
+        # @api public
+        def run(request_action, path, params = {}, headers = {})
+          if %i[post patch put].include?(request_action.to_sym)
+            body = params
+          else
+            request_params = params
+          end
+          __run(request_action, path,
+                params: request_params,
+                headers: headers,
+                body: body)
+        end
+      else
+        # @see __run
+        alias_method :run, :__run
       end
 
 
       private
 
-      def request_headers(req_headers = {})
-        headers = connection_options[:headers] || {}
-        headers = headers.merge(req_headers)
-        if Shark.service_token.present?
-          headers["Authorization"] = "Bearer #{Shark.service_token}"
-        end
-
-        headers
+      def connection_options_headers
+        connection_options[:headers] || {}
       end
 
-      def request_params(params = {})
-        (connection_options[:params] || {}).merge(params || {})
+      def connection_options_params
+        connection_options[:params] || {}
       end
     end
   end

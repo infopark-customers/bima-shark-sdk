2eb54a369b8ec13179065c56a9db1f00bfe7880d
Huy Dinh <huy.dinh@infopark.de>
2019-04-10 01:22:07 +0200

    Add `Shark::MailingService`.

---
+++ lib/shark/mailing_service.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'shark/mailing_service/base'
+require 'shark/mailing_service/mail'
+
+module Shark
+  module MailingService
+  end
+end
---
+++ lib/shark/mailing_service/mail.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Shark
+  module MailingService
+    class Mail < Base
+    end
+  end
+end

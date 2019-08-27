58aca69ac52e8cbc0b6cb793b4cb7d9321923cc7
Joergen Dahlke <joergen.dahlke@infopark.de>
2019-08-27 17:46:03 +0200

    cleanup SharkSpec helpers with some meta programming

--- lib/bima-shark-sdk/rspec.rb
+++ lib/bima-shark-sdk/rspec.rb
@@ -1,9 +1,32 @@
-require File.expand_path("../../shark/rspec/helpers", __FILE__)
+require File.expand_path('../../shark/rspec/helpers', __FILE__)
 
 RSpec.configure do |config|
 
 end
 
-module SharkSpec
+class SharkSpec
   extend Shark::RSpec::Helpers
+
+  class << self
+    def method_missing(method, *args, &block)
+      if method.match(/^stub_/)
+        fake_service(method).setup
+      elsif method.match(/^unstub_/)
+        fake_service(method).reset
+      else
+        super
+      end
+    end
+
+    protected
+    def fake_service(method)
+      service = method.to_s.gsub(/(un)?stub_/, '')
+      service = camelize(service)
+      Module.const_get("Shark::RSpec::Fake#{service}")
+    end
+
+    def camelize(string, delimiter = '_')
+      string.split(delimiter).map(&:capitalize).join
+    end
+  end
 end

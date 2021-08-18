b9b456f2a71a39ed5b6e129ab349e1135ee70032
Johannes Schmeißer <johannes.schmeisser@infopark.de>
2021-08-19 01:29:29 +0200

    added Shark::Contact.find_by_permissions

--- lib/shark/contact.rb
+++ lib/shark/contact.rb
@@ -11,11 +11,24 @@ module Shark
 
     has_many :memberships
     has_many :groups
+    has_one :permission
 
     def self.find_by_email(email)
       normalized_email = normalize_email(email)
       options = { filter: { equals: { email: normalized_email } } }
       where(options).first
+    end
+
+    # @example
+    #   contacts = Shark::ContactService::Contact.find_by_permissions([
+    #     { resource: 'paragraph::contract', privilege: 'admin' },
+    #     { resource: 'datenraum', privilege: 'admin'}
+    #   ])
+    #
+    # @param permissions_filter [Array] the filter of resources and privileges.
+    # @api public
+    def self.find_by_permissions(permissions_filter)
+      self.includes('permission').where(permissions: permissions_filter)
     end
 
     def account
